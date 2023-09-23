3b955125684afe43d54a1fef5a545593b89a71d2
Mustafa Dalga <mustafadalgaa@gmail.com>
2023-09-23 11:34:56 +0300

    Game screen transition effect has been developed

---
+++ app/_components/GameScreen.tsx
@@ -0,0 +1,29 @@
+"use client";
+import GameOptions from "@/_components/GameOptions";
+import useGame from "@/_providers/game/useGame";
+import GamePyramidContainer from "@/_components/GamePyramidContainer";
+import { CSSTransition, SwitchTransition } from "react-transition-group";
+import "./gameScreenStyle.css"
+import { useRef } from "react";
+
+export default function GameScreen() {
+    const { showGame } = useGame();
+    const nodeRef = useRef<HTMLDivElement | null>(null)
+    return (
+        <SwitchTransition mode="out-in">
+            <CSSTransition
+                key={showGame ? 'GamePyramidContainer' : 'GameOptions'}
+                nodeRef={nodeRef}
+                addEndListener={(done) => {
+                    nodeRef.current?.addEventListener("transitionend", done, false);
+                }}
+                classNames="fade">
+
+
+                <div ref={nodeRef} className="w-full flex justify-center">
+                    {showGame ? <GamePyramidContainer/> : <GameOptions/>}
+                </div>
+            </CSSTransition>
+        </SwitchTransition>
+    )
+};
