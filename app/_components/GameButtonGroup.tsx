096131f1159abf7d4a2f2d342c7a3fc265d6c2c4
Mustafa Dalga <mustafadalgaa@gmail.com>
2023-09-23 11:34:25 +0300

    Pyramid button options and functionality has been added

---
+++ app/_components/GameButtonGroup.tsx
@@ -0,0 +1,38 @@
+import useGame from "@/_providers/game/useGame";
+import findSolutions from "@/_utilities/findSolutions";
+import { Path } from "@/_types";
+
+export default function GameButtonGroup() {
+    const { pyramid, dimension, setPyramid, newGame, setShowGame, setGamerPath } = useGame();
+    const handleShowSolution = () => {
+        const solution = findSolutions(pyramid, dimension);
+        const firstSolution: Path = solution[0];
+        if (firstSolution) {
+            setGamerPath(firstSolution)
+        }
+    }
+    const handleExit = () => {
+        setGamerPath([]);
+        setPyramid([]);
+        setShowGame(false)
+    }
+    return (
+        <div className="my-10 flex justify-center gap-3">
+            <button type="button"
+                    onClick={newGame}
+                    className="bg-indigo-500 hover:bg-indigo-600 active:bg-indigo-700 text-white text-center text-xs sm:text-sm gap-3 min-w-[5rem] lg:min-w-0 px-3 py-1 rounded-md shadow transition-all truncate">
+                <span>New Game</span>
+            </button>
+            <button type="button"
+                    onClick={handleShowSolution}
+                    className="bg-indigo-500 hover:bg-indigo-600 active:bg-indigo-700 text-white text-center text-xs sm:text-sm gap-3 min-w-[5rem] lg:min-w-0 px-3 py-1 rounded-md shadow transition-all truncate">
+                <span>Show Solution</span>
+            </button>
+            <button type="button"
+                    onClick={handleExit}
+                    className="bg-indigo-500 hover:bg-indigo-600 active:bg-indigo-700 text-white text-center text-xs sm:text-sm gap-3 min-w-[5rem] lg:min-w-0 px-3 py-1 rounded-md shadow transition-all truncate">
+                <span>Exit</span>
+            </button>
+        </div>
+    );
+};
